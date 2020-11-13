e51135c98df9e3da1b5e80a6ddf2998643c9d004
Alex <alexlzrw@gmail.com>
2020-11-13 22:05:56 +0300

    movement player and flip sprite

---
+++ TIleVania/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    Rigidbody2D myRigidbody;
+
+    [SerializeField] float runSpeed = 5f;
+
+    private void Start() {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        MovementPlayer();
+        FlipSprite();
+    }
+
+    private void MovementPlayer() {
+        float controlThrow = Input.GetAxisRaw("Horizontal") * runSpeed;
+        Vector2 velosityPlayer = new Vector2(controlThrow, myRigidbody.velocity.y);
+        myRigidbody.velocity = velosityPlayer;
+    }
+
+    private void FlipSprite() {
+        bool horizontalSpeedPlayer = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+
+        if (horizontalSpeedPlayer) {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
+    }
+
+}
